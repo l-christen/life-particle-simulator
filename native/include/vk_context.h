046c474d96e543542545b9c6c64e8f7007c16ac3
Loic <loicchristenn@gmail.com>
2025-11-28 15:32:19 +0100

    Cuda-Vulkan Interop : Added the physical device, the VkQueue and the queue family to the VkContext because they are needed to copy a native vulkan buffer into the Godot buffer

--- native/include/vk_context.h
+++ native/include/vk_context.h
@@ -8,7 +8,10 @@ namespace godot {
     class VkContext {
         private:
             // Native Vulkan device handle kept private to avoid direct access
+            VkPhysicalDevice physical_device = VK_NULL_HANDLE;
             VkDevice device = VK_NULL_HANDLE;
+            VkQueue queue = VK_NULL_HANDLE;
+            uint32_t queue_family_index = 0;
             VkContext() = default;
         public:
             // Singleton instance
@@ -16,12 +19,24 @@ namespace godot {
                 static VkContext instance;
                 return instance;
             }
-            // Check if Vulkan device is initialized
-            bool initialized() const { return device != VK_NULL_HANDLE; }
+            // Check if Vulkan device is initialized (we don't check for queue family index since it can be 0)
+            bool initialized() const { return device != VK_NULL_HANDLE && queue != VK_NULL_HANDLE && physical_device != VK_NULL_HANDLE; }
 
+            // Getter and Setter for VkPhysicalDevice
+            VkPhysicalDevice get_physical_device() const { return physical_device; }
+            void set_physical_device(VkPhysicalDevice phys_dev) { physical_device = phys_dev; }
+            
             // Getter and Setter for VkDevice
             VkDevice get_device() const { return device; }
             void set_device(VkDevice dev) { device = dev; }
+
+            // Getter and Setter for VkQueue
+            VkQueue get_queue() const { return queue; }
+            void set_queue(VkQueue q) { queue = q; }
+
+            // Getter and Setter for queue family index
+            uint32_t get_queue_family_index() const { return queue_family_index; }
+            void set_queue_family_index(uint32_t index) { queue_family_index = index; }
 
             // Vulkan initialization method : This method is called from _process because in _ready the rendering device is not yet ready
             bool init(RenderingServer *rs);
--- native/src/vk_context.cpp
+++ native/src/vk_context.cpp
@@ -33,9 +33,47 @@ bool VkContext::init(RenderingServer *rs) {
         return false;
     }
 
+    // Get Vulkan physical device from RenderingDevice
+    uint64_t raw_physical_device = rd->get_driver_resource(
+        RenderingDevice::DRIVER_RESOURCE_VULKAN_PHYSICAL_DEVICE,
+        RID(),
+        0
+    );
+    if (raw_physical_device == 0) {
+        UtilityFunctions::print("Failed to fetch Vulkan physical device handle!");
+        return false;
+    }
+
+    // Get Vulkan queue from RenderingDevice
+    uint64_t raw_queue = rd->get_driver_resource(
+        RenderingDevice::DRIVER_RESOURCE_VULKAN_QUEUE,
+        RID(),
+        0
+    );
+    if (raw_queue == 0) {
+        UtilityFunctions::print("Failed to fetch Vulkan queue handle!");
+        return false;
+    }
+
+    // Get queue family index from RenderingDevice
+    uint64_t queue_family_index = rd->get_driver_resource(
+        RenderingDevice::DRIVER_RESOURCE_VULKAN_QUEUE_FAMILY_INDEX,
+        RID(),
+        0
+    );
+
+    // Store Vulkan physical device handle
+    this->set_physical_device(reinterpret_cast<VkPhysicalDevice>(raw_physical_device));
     // Store Vulkan device handle
     this->set_device(reinterpret_cast<VkDevice>(raw_device));
+    // Store Vulkan queue handle
+    this->set_queue(reinterpret_cast<VkQueue>(raw_queue));
+    // Store queue family index
+    this->set_queue_family_index((uint32_t)queue_family_index);
     // Print Vulkan device handle, we need to cast it to uint64_t for printing
+    UtilityFunctions::print("Vulkan physical device OK: ", (uint64_t)this->get_physical_device());
     UtilityFunctions::print("Vulkan device OK: ", (uint64_t)this->get_device());
-    return true;
+    UtilityFunctions::print("Vulkan queue OK: ", (uint64_t)this->get_queue());
+    UtilityFunctions::print("Vulkan queue family index OK: ", this->get_queue_family_index());
+    return this->initialized();
 }
