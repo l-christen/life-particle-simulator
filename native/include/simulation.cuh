60dae5a21c7b52e6c617643dab25ede7978338b8
Loicc <loicchristenn@gmail.com>
2025-12-04 11:22:20 +0100

    Cuda simulation : Interface functions for GDExtension declared in this header file.

---
+++ native/include/simulation.cuh
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "particlesSoA.cuh"
+#include "particlesAoS.cuh"
+
+// This file declares the interface for the simulation functions
+// inspired by P.A. Mudry ray tracer
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void initSimulation(
+    ParticlesSoA* prev,
+    ParticlesSoA* next,
+    ParticlesAoS* render,
+    const float* rules,
+    const float* radiusOfInfluence,
+    int numParticles,
+    int numTypes,
+    float width,
+    float height
+);
+
+void runSimulationStep(
+    ParticlesSoA* prev,
+    ParticlesSoA* next,
+    ParticlesAoS* render,
+    int numParticles,
+    float width,
+    float height,
+    float deltaTime,
+    cudaEvent_t simulation_done
+);
+
+#ifdef __cplusplus
+}
+#endif
