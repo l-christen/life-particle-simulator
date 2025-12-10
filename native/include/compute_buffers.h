6a24f0cf9abbe1bcae6ce7666d45dd2307c43673
Loic <loicchristenn@gmail.com>
2025-12-10 17:08:05 +0100

    Cuda simulation : Implementation of ComputeBuffers now in a cpp file in order to include cuda runtime in the cpp files because cuda runtime might not work well with godot while compiling the extension.

--- native/include/compute_buffers.h
+++ native/include/compute_buffers.h
@@ -4,9 +4,8 @@
 #include "particlesAoS.cuh"
 #include "particle.h"
 
-#include <cuda_runtime.h>
+#include <utility>
 #include <cstdint>
-#include <cstdio>
 #include <cstdlib>
 
 class ComputeBuffers {
@@ -16,59 +15,9 @@ public:
     ParticlesSoA next;
     ParticlesAoS renderBuffer; // buffer for rendering (AoS format)
 
-    // Constructor that allocates device memory for compute buffers
-    ComputeBuffers(uint32_t max_particles) {
-        capacity = max_particles;
-
-        // Allocate prev buffers
-        cudaMalloc(&prev.d_x, sizeof(float) * capacity);
-        cudaMalloc(&prev.d_y, sizeof(float) * capacity);
-        cudaMalloc(&prev.d_vx, sizeof(float) * capacity);
-        cudaMalloc(&prev.d_vy, sizeof(float) * capacity);
-        cudaMalloc(&prev.d_type, sizeof(uint32_t) * capacity);
-
-        // Allocate next buffers
-        cudaMalloc(&next.d_x, sizeof(float) * capacity);
-        cudaMalloc(&next.d_y, sizeof(float) * capacity);
-        cudaMalloc(&next.d_vx, sizeof(float) * capacity);
-        cudaMalloc(&next.d_vy, sizeof(float) * capacity);
-
-        // Allocate render buffer
-        cudaMalloc(&renderBuffer.d_particles, sizeof(Particle) * capacity);
-
-        // Allocate host memory for render buffer
-        renderBuffer.h_particles = new Particle[capacity];
-
-        next.d_type = prev.d_type; // shared between prev and next
-
-        prev.capacity = capacity;
-        next.capacity = capacity;
-        renderBuffer.capacity = capacity;
-
-        // Initialize number of particles to zero, will be set during initialization
-        prev.numParticles = 0;
-        next.numParticles = 0;
-        renderBuffer.numParticles = 0;
-    }
-
-    // Destructor that frees device memory
-    ~ComputeBuffers() {
-        cudaFree(prev.d_x);
-        cudaFree(prev.d_y);
-        cudaFree(prev.d_vx);
-        cudaFree(prev.d_vy);
-
-        cudaFree(prev.d_type); // shared between prev and next
-
-        cudaFree(next.d_x);
-        cudaFree(next.d_y);
-        cudaFree(next.d_vx);
-        cudaFree(next.d_vy);
-
-        cudaFree(renderBuffer.d_particles);
-
-        delete[] renderBuffer.h_particles;
-    }
+    // Constructor and destructor
+    ComputeBuffers(uint32_t max_particles);
+    ~ComputeBuffers();
 
     // Swap prev and next buffers when a simulation step is complete (better than copying data)
     void swap() {
@@ -76,5 +25,6 @@ public:
     }
 
 private:
+    // Maximum capacity of particles
     uint32_t capacity;
 };
---
+++ native/src/compute_buffers.cpp
@@ -0,0 +1,59 @@
+#include "compute_buffers.h"
+
+#include <cuda_runtime.h>
+
+// This file was made to avoid indirect cuda runtime includes in GDExtension
+
+// Constructor that allocates device memory for compute buffers
+ComputeBuffers::ComputeBuffers(uint32_t max_particles) {
+    capacity = max_particles;
+
+    // Allocate prev buffers
+    cudaMalloc(&prev.d_x, sizeof(float) * capacity);
+    cudaMalloc(&prev.d_y, sizeof(float) * capacity);
+    cudaMalloc(&prev.d_vx, sizeof(float) * capacity);
+    cudaMalloc(&prev.d_vy, sizeof(float) * capacity);
+    cudaMalloc(&prev.d_type, sizeof(uint32_t) * capacity);
+
+    // Allocate next buffers
+    cudaMalloc(&next.d_x, sizeof(float) * capacity);
+    cudaMalloc(&next.d_y, sizeof(float) * capacity);
+    cudaMalloc(&next.d_vx, sizeof(float) * capacity);
+    cudaMalloc(&next.d_vy, sizeof(float) * capacity);
+
+    // Allocate render buffer
+    cudaMalloc(&renderBuffer.d_particles, sizeof(Particle) * capacity);
+
+    // Allocate host memory for render buffer
+    renderBuffer.h_particles = new Particle[capacity];
+
+    next.d_type = prev.d_type; // shared between prev and next
+
+    prev.capacity = capacity;
+    next.capacity = capacity;
+    renderBuffer.capacity = capacity;
+
+    // Initialize number of particles to zero, will be set during initialization
+    prev.numParticles = 0;
+    next.numParticles = 0;
+    renderBuffer.numParticles = 0;
+}
+
+// Destructor that frees device memory
+ComputeBuffers::~ComputeBuffers() {
+    cudaFree(prev.d_x);
+    cudaFree(prev.d_y);
+    cudaFree(prev.d_vx);
+    cudaFree(prev.d_vy);
+
+    cudaFree(prev.d_type); // shared between prev and next
+
+    cudaFree(next.d_x);
+    cudaFree(next.d_y);
+    cudaFree(next.d_vx);
+    cudaFree(next.d_vy);
+
+    cudaFree(renderBuffer.d_particles);
+
+    delete[] renderBuffer.h_particles;
+}
